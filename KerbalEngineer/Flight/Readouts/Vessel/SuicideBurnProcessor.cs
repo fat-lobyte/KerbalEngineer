cead4f59283c038682ee5d773ac8d5bf3461c6e8
Alexander Korsunsky <fat.lobyte9@gmail.com>
2017-04-04 13:03:51 +0200

    SuicideBurnProcessor: Initial implementation of numeric altitude calculation

--- KerbalEngineer/Flight/Readouts/Vessel/SuicideBurnProcessor.cs
+++ KerbalEngineer/Flight/Readouts/Vessel/SuicideBurnProcessor.cs
@@ -1,7 +1,7 @@
 ﻿// 
 //     Kerbal Engineer Redux
 // 
-//     Copyright (C) 2014 CYBUTEK
+//     Copyright (C) 2014 CYBUTEK, 2017 fat-lobyte
 // 
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
@@ -19,12 +19,12 @@
 
 namespace KerbalEngineer.Flight.Readouts.Vessel
 {
+    using Surface;
     using System;
 
     public class SuicideBurnProcessor : IUpdatable, IUpdateRequest
     {
         private static readonly SuicideBurnProcessor s_Instance = new SuicideBurnProcessor();
-        private double m_Acceleration;
         private double m_Gravity;
         private double m_RadarAltitude;
 
@@ -48,29 +48,91 @@ namespace KerbalEngineer.Flight.Readouts.Vessel
 
         public void Update()
         {
-            if (FlightGlobals.currentMainBody == null || FlightGlobals.ActiveVessel == null || SimulationProcessor.LastStage == null ||
-                FlightGlobals.ship_orbit.PeA >= 0.0 || !SimulationProcessor.ShowDetails)
+            var vessel = FlightGlobals.ActiveVessel;
+            CelestialBody body = vessel?.mainBody;
+
+            if (FlightGlobals.currentMainBody == null || vessel == null ||
+                SimulationProcessor.LastStage == null || !SimulationProcessor.ShowDetails ||
+                FlightGlobals.ship_orbit.PeA >= 0.0 || !Surface.ImpactProcessor.ShowDetails)
             {
                 ShowDetails = false;
                 return;
             }
 
-            m_Gravity = FlightGlobals.currentMainBody.gravParameter / Math.Pow(FlightGlobals.currentMainBody.Radius, 2.0);
-            m_Acceleration = SimulationProcessor.LastStage.thrust / SimulationProcessor.LastStage.totalMass;
+            // get time of impact
+            double impactUT = ImpactProcessor.Time + Planetarium.GetUniversalTime();
+
+            // get orbital velocity and orbital position at time of impact
+            Vector3d orbitalVelocityAtImpact, orbitalPosAtImpact;
+            vessel.orbit.GetOrbitalStateVectorsAtUT(impactUT, out orbitalPosAtImpact, out orbitalVelocityAtImpact);
+
+            // calculate impact angle and impact velocity considering the planet rotation
+            Vector3d velocityAtImpact = orbitalVelocityAtImpact - vessel.orbit.GetRotFrameVelAtPos(body, orbitalPosAtImpact);
+            double angleToVertical = Vector3d.Angle(-orbitalPosAtImpact, velocityAtImpact) * Math.PI / 180.0;
+            double speedAtImpact = velocityAtImpact.magnitude;
+
+
+            // gravity at impact
+            m_Gravity = FlightGlobals.currentMainBody.gravParameter /
+                           Math.Pow(FlightGlobals.currentMainBody.Radius + ImpactProcessor.Altitude, 2.0);
+
             m_RadarAltitude = FlightGlobals.ActiveVessel.terrainAltitude > 0.0
                 ? FlightGlobals.ship_altitude - FlightGlobals.ActiveVessel.terrainAltitude
                 : FlightGlobals.ship_altitude;
 
-            DeltaV = Math.Sqrt((2 * m_Gravity * m_RadarAltitude) + Math.Pow(FlightGlobals.ship_verticalSpeed, 2.0));
-            Altitude = Math.Pow(DeltaV, 2.0) / (2.0 * m_Acceleration);
-            Distance = m_RadarAltitude - Altitude;
 
-            Countdown = Surface.ImpactProcessor.Time - 0.5 *
-                (SimulationProcessor.LastStage.time / SimulationProcessor.LastStage.deltaV * DeltaV);
+            // Sharaf, M.A., & Alaqal, L.A.(2012).Computational Algorithm for Gravity Turn Maneuver, 12 (13).
+
+            // calculate TWR at the impact point
+            double n = SimulationProcessor.LastStage.thrust / (SimulationProcessor.LastStage.mass * m_Gravity);
+                
+               // SimulationProcessor.LastStage.thrustToWeight;
+            double z0 = Math.Tan(0.5 * angleToVertical);
+            double C = speedAtImpact / (Math.Pow(z0, n - 1.0) * (1 + (z0*z0)));
+
+            double decelTime = C / m_Gravity * Math.Pow(z0, n - 1.0) * (1.0 / (n - 1.0) + z0 * z0 / (n + 1.0));
+
+            // we simulate by varying the z parameter, and we split it up in N_STEPS.
+            uint N_STEPS = 100;
+            double z_step = z0 / N_STEPS;
+            double psi_step = angleToVertical / N_STEPS;
+
+            // simulation variables
+            double psi, z, v, t, dt;
+            double x = 0.0, y = 0.0;
+
+
+            double t_prev = decelTime;
+            double psi_prev = angleToVertical;
+            double v_prev = speedAtImpact;
+
+            // suicide burn simulation loop
+            //for (double z = z0; z >= 0.0; z -= z_step)
+            for (psi = angleToVertical; psi >= 0.0; psi -= psi_step)
+            {
+                //psi = 2 * Math.Atan(z);
+                z = Math.Tan(0.5 * psi);
+                v = C * Math.Pow(z, n - 1.0) * (1.0 + z * z);
+                t = C / m_Gravity * Math.Pow(z, n - 1.0) * (1.0/(n - 1.0) + z*z / (n + 1.0));
+
+                dt = t_prev - t;
+                x += 0.5 * (v * Math.Sin(psi) + v_prev * Math.Sin(psi_prev)) * dt;
+                y += 0.5 * (v * Math.Cos(psi) + v_prev * Math.Cos(psi_prev)) * dt; 
+
+                t_prev = t;
+                psi_prev = psi;
+                v_prev = v;
+            }
+
+            // store results
+            Countdown = ImpactProcessor.Time - 0.5 * decelTime;
+            DeltaV = decelTime / SimulationProcessor.LastStage.time * SimulationProcessor.LastStage.deltaV;
+            Altitude = ImpactProcessor.Altitude + y;
+            Distance = m_RadarAltitude - Altitude;
 
             ShowDetails = !double.IsInfinity(Distance);
         }
-
+        
         public bool UpdateRequested { get; set; }
 
         public static void RequestUpdate()
@@ -81,6 +143,7 @@ namespace KerbalEngineer.Flight.Readouts.Vessel
 
         public static void Reset()
         {
+            FlightEngineerCore.Instance.AddUpdatable(ImpactProcessor.Instance);
             FlightEngineerCore.Instance.AddUpdatable(SimulationProcessor.Instance);
             FlightEngineerCore.Instance.AddUpdatable(s_Instance);
         }
